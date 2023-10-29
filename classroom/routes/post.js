7d7b5dfea4a88f8210445871eff8d28ab8338d68
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2023-10-29 20:42:37 +0530

    Add files via upload

---
+++ classroom/routes/post.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+//Index
+router.get("/", (req, res) => {
+  res.send("GET for users");
+});
+
+//Show
+router.get("/:id", (req, res) => {
+  res.send("GET for post id");
+});
+
+//POST
+router.post("/", (req, res) => {
+  res.send("POST for posts");
+});
+
+//DELETE
+router.delete("/:id", (req, res) => {
+  res.send("DELETE for post id");
+});
+
+module.exports = router;
---
+++ public/js/map.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  center: listing.geometry.coordinates, // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
+
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${listing.title}</h4><p>Exact loaction will be provided after booking</p>`
+    )
+  )
+  .addTo(map);
