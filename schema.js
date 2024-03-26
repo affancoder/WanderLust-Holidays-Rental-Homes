f0499a88454be69a485177a2d5b1d9f605904ddc
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-03-26 19:41:09 +0530

    Add files via upload

--- schema.js
+++ schema.js
@@ -6,6 +6,7 @@ module.exports.listingSchema = Joi.object({
     description: Joi.string().required(),
     location: Joi.string().required(),
     country: Joi.string().required(),
+    category: Joi.string(),
     price: Joi.number().required().min(0),
     image: Joi.string().allow("", null),
   }).required(),
